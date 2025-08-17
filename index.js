26be74f48b85681e8fa125620e27d0e2de1e8c71
AbrahamZapata <joseabrahamzapatacruz@gmail.com>
2025-08-16 22:17:58 -0500

    desactivate cors

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({
+/*app.use(cors({
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
       "https://TU-FRONT.vercel.app"  // pon tu dominio vercel
     ]
   }));
+*/
+  
+app.use(cors({ origin: "*" }));
 
 const { Pool } = pkg;
 const pool = new Pool({
