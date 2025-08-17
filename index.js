0045afe13387a85016bf210b3425cfa89519e619
AbrahamZapata <joseabrahamzapatacruz@gmail.com>
2025-08-16 22:14:32 -0500

    first Endpoint test

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "*" })); // luego limítalo a tu dominio de Vercel
+app.use(cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://TU-FRONT.vercel.app"  // pon tu dominio vercel
+    ]
+  }));
 
 const { Pool } = pkg;
 const pool = new Pool({
@@ -24,6 +30,37 @@ app.get("/health", async (_req, res) => {
     res.status(500).json({ ok: false, error: e.message });
   }
 });
+
+// LISTAR pacientes
+app.get("/api/pacientes", async (_req, res) => {
+    try {
+      const r = await pool.query(
+        "select id, nombre, especie, raza, fecha_nacimiento from pacientes order by nombre asc"
+      );
+      res.json(r.rows);
+    } catch (e) {
+      console.error("DB ERROR /api/pacientes:", e);
+      res.status(500).json({ error: e.message });
+    }
+  });
+  
+  // CREAR paciente
+  app.post("/api/pacientes", async (req, res) => {
+    const { nombre, especie, raza, fecha_nacimiento } = req.body || {};
+    if (!nombre) return res.status(400).json({ error: "nombre requerido" });
+    try {
+      const r = await pool.query(
+        `insert into pacientes (nombre, especie, raza, fecha_nacimiento)
+         values ($1,$2,$3,$4) returning id, nombre, especie, raza, fecha_nacimiento`,
+        [nombre, especie ?? null, raza ?? null, fecha_nacimiento ?? null]
+      );
+      res.status(201).json(r.rows[0]);
+    } catch (e) {
+      console.error("DB ERROR POST /api/pacientes:", e);
+      res.status(500).json({ error: e.message });
+    }
+  });
+  
 
 app.listen(process.env.PORT || 3000, () =>
   console.log(`API corriendo en puerto ${process.env.PORT || 3000}`)
